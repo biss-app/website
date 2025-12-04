6933592a68b762d1e7566ffc0ba79c34db723eb2
biss-app <contact@biss-app.fr>
2025-12-04 14:45:25 +0100

    new stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,8 +4,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error("La clé secrète Stripe (STRIPE_SECRET_KEY) n'est pas configurée.");
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-11-17.clover",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
